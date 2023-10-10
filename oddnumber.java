c94486c7f5a00fb064774c98c14ead0fdae2a200
Sai Punde <147430689+saipunde159@users.noreply.github.com>
2023-10-10 16:26:09 +0530

    Create oddnumber.java

---
+++ oddnumber.java
@@ -0,0 +1,10 @@
+public class oddnumber{
+  public static void main(String[] args){
+    int n = 13;
+    if(n%2!=0){
+      System.out.println("Odd Number");
+    }else{
+      System.out.println("Not Odd Number");
+    } 
+  }
+}
